3e1394247d0dbd7acb675063decad657cda92fa4
Jens Låås <jens.laas@uadm.uu.se>
2012-10-30 16:42:39 +0100

    properly split param config

--- kp.js
+++ kp.js
@@ -195,9 +195,10 @@ function job_new(name) {
     job.param_display = function (name, elem, param) {
 	var arr = param.split(':');
 	elem.append("<b>" + arr[0] + ":</b> ");
-	arr = arr[1].split('\n');
+	arr = param.substring(param.indexOf(":")).split('\n');
 	arr = arr[0].split(' ');
 	arr = jQuery.grep( arr, function (e) { return e.length > 0; } );
+	arr.shift();
 	if(arr[0] == "text") {
 	    elem.append('<input type="text" maxlength="8" size="8" name="'+name+'">');
 	    this[name+'_value'] = function () {
